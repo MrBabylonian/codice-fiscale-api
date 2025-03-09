b90937d1af70108854aa5387cf037ffdc085bda5
MrBabylonian <bedirhangilgiler@gmail.com>
2025-03-09 16:16:48 +0100

    Production ready

--- src/main/java/com/codicefiscaleapi/config/WebConfig.java
+++ src/main/java/com/codicefiscaleapi/config/WebConfig.java
@@ -10,11 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
+                .allowedOrigins("*")  // Try this instead of allowedOriginPatterns
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("*")
-                .allowCredentials(false)
+                .exposedHeaders("Access-Control-Allow-Origin")
                 .maxAge(3600);
     }
 }
